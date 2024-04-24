b7eb81094e3129d133a456e05c8b2062fdea6277
Maheen Khan <maheeenkhan4@gmail.com>
2024-04-24 22:41:40 +0500

    First

---
+++ insert-search-bst.cpp
@@ -0,0 +1,69 @@
+// Binary Search Tree. 
+//Implement Insertion and Searching Algorithms.
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+struct BstNode {
+	int data; 
+	BstNode* left;
+	BstNode* right;
+};
+
+// Function to create a new Node in heap
+BstNode* GetNewNode(int data) {
+	BstNode* newNode = new BstNode();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+// To insert data in BST, returns address of root node 
+BstNode* Insert(BstNode* root,int data) {
+	if(root == NULL) { // empty tree
+		root = GetNewNode(data);
+	}
+	// if data to be inserted is lesser, insert in left subtree. 
+	else if(data <= root->data) {
+		root->left = Insert(root->left,data);
+	}
+	// else, insert in right subtree. 
+	else {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+//Search function
+bool Search(BstNode* root,int data) {
+	if(root == NULL) {
+		return false;
+	}
+	else if(root->data == data) {
+		return true;
+	}
+	else if(data <= root->data) {
+		return Search(root->left,data);
+	}
+	else {
+		return Search(root->right,data);
+	}
+}
+int main() {
+	BstNode* root = NULL;  // Creating an empty tree
+	/*Code to test the logic*/
+	root = Insert(root,15);	
+	root = Insert(root,10);	
+	root = Insert(root,20);
+	root = Insert(root,25);
+	root = Insert(root,8);
+	root = Insert(root,12);
+	
+	 
+	int number;
+	cout<<"Enter number be searched\n";
+	cin>>number;
+	
+	if(Search(root,number) == true) cout<<"Found\n";
+	else cout<<"Not Found\n";
+}
